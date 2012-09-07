276a9913d75f49556710c893fb2592944e93b83e
Hongwei Wang <hww712@gmail.com>
2012-09-07 13:13:32 +0800

    add

---
+++ algorithm/binary-tree/bstree.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+binary search tree
+
+*/
+
+typedef struct binary_search_tree {
+    int                       data;
+    struct binary_search_tree *lchild;
+    struct binary_search_tree *rchild;
+} *bstree;
+
+
+bstree create();
+bstree insert(bstree, int);
+bstree search(bstree, int);
+
+/* ldr: left-middle-right */
+/* dlr: middle-left-right */
+void traverse_ldr(bstree);
+void traverse_dlr(bstree);
+
+
+void destroy(bstree);
+void delete(bstree, int);
+
+/*
+void destroy(bstree);
+void delete(bstree, int);
+bstree search(bstree, char *);
+void pop(bstree);
+void push(bstree, char *);
+*/
+
+void delete(bstree root, int data)
+{
+    btree getroot = search(root, data)
+    if (getroot->rchild == NULL && getroot->lchild == NULL)
+    {
+        getroot = NULL
+    }
+    else if(getroot->rchild = NULL || getroot->lchild == NULL)
+    {
+        getroot->rchild
+    }
+}
+
+bstree find_parent(bstree root)
+{
+    
+}
+
+void destroy(bstree root)
+{
+    if(root != NULL)
+    {
+        destroy(root->lchild);
+        destroy(root->rchild);
+        free(root);
+    }
+}
+
+bstree search(bstree root, int data)
+{
+    if(root == NULL)
+    {
+        printf("root is NULL\n", data);
+    }
+    else if( data < (root->data) )
+    {
+        printf("left:  ");
+        root = search(root->lchild, data);
+    }
+    else if( data > (root->data) )
+    {
+        printf("right: ");
+        root = search(root->rchild, data);
+    }
+    else
+    {
+        printf("find, root->data=[%d]\n", root->data);
+    }
+    
+    return root;
+}
+
+bstree create()
+{
+    printf("create()\n\n");
+    
+    bstree root = NULL;
+    
+    return root;
+}
+
+bstree insert(bstree root, int data)
+{
+    if(root == NULL)
+    {
+        root = malloc(sizeof(bstree));
+        
+        root->data   = data;
+        root->lchild = NULL;
+        root->rchild = NULL;
+        
+        printf("[%d]\n", data);
+    }
+    else if( data < (root->data) )
+    {
+        printf("left  ");
+        root->lchild = insert(root->lchild, data);
+    }
+    else
+    {
+        printf("right ");
+        root->rchild = insert(root->rchild, data);
+    }
+    
+    return root;
+}
+
+void traverse_ldr(bstree root)
+{
+    if(root != NULL)
+    {
+        traverse_ldr(root->lchild);
+        printf("DATA: %d\n", root->data);
+        traverse_ldr(root->rchild);
+    }
+}
+
+void traverse_dlr(bstree root)
+{
+    if(root != NULL)
+    {
+        printf("DATA: %d\n", root->data);
+        traverse_dlr(root->lchild);
+        traverse_dlr(root->rchild);
+    }
+}
+
+int main()
+{
+    bstree root = create();
+    
+    printf("insert:\n");
+    root = insert(root, 3);
+    root = insert(root, 1);
+    root = insert(root, 2);
+    root = insert(root, 5);
+    root = insert(root, 7);
+    root = insert(root, 22);
+    root = insert(root, 4);
+    printf("\n");
+    
+    printf("traverse_ldr(bstree):\n");
+    traverse_ldr(root);
+    printf("\n");
+    
+    printf("traverse_dlr(bstree):\n");
+    traverse_dlr(root);
+    printf("\n");
+    
+    bstree temp;
+    temp = search(root, 4);
+    
+    destroy(root);
+    
+}
---
+++ c/valist/valist.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+typedef char * va_list;
+
+#define _INTSIZEOF(n) ((sizeof(n) + sizeof(int) - 1)  &  ~(sizeof(int)-1))
+#define va_start(ap, v) (ap = (va_list)&v + _INTSIZEOF(v))
+#define va_arg(ap, t) ( *( t * ) ((ap += _INTSIZEOF(t)) - _INTSIZEOF(t)) )
+#define va_end(ap)  (ap = (va_list)0)
+
+
+void mprintf(char *, ...);
+
+int main()
+{
+    int i;
+    char *s;
+    
+    mprintf("test start...\n");
+    
+    i = 1;
+    s = "hello world!";
+    mprintf("i=%d, s=%s\n", i, s);
+    
+    s = "second test!";
+    mprintf("s=%s\n", s);
+}
+
+void mprintf(char *fmt, ...)
+{
+    char *p, *sval;
+    int ival;
+    
+    va_list ap;
+    
+    va_start(ap, fmt);
+    
+    for (p=fmt; *p; p++)
+    {
+        if (*p != '%')
+        {
+            putchar(*p);
+            continue;
+        }
+        
+        switch (*++p)
+        {
+            case 'd':
+                ival = va_arg(ap, int);
+                printf("%d", ival);
+                break;
+                
+            case 's':
+                for ( sval=va_arg(ap, char *); *sval; *sval++)
+                    putchar(*sval);
+                break;
+                
+            default:
+                putchar(*p);
+                break;
+        }
+    }
+    
+    va_end(ap);
+}
+
