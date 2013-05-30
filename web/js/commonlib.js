53b47791f4fa9279126bff341977813aa6dcb322
Hongwei Wang <hww712@gmail.com>
2013-05-30 01:58:29 -0700

    Scrollup and list all finished.

---
+++ web/js/commonlib.js
@@ -0,0 +1,34 @@
+﻿
+
+function OnMouseOver(elmt)
+{
+    //alert('h呵呵aha');
+	//elmt.parentNode.parentNode.parentNode.style.backgroundColor = '#E9E9E9';
+}
+
+
+
+/*
+scrollUp
+*/
+$(document).ready(function(){ 
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollup').fadeIn();
+		} else {
+			$('.scrollup').fadeOut();
+		}
+	}); 
+
+	$('.scrollup').click(function(){
+		$("html, body").animate({ scrollTop: 0 }, 600);
+		return false;
+	});
+
+});
+
+
+
+
+
