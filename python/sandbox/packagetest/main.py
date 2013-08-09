f76bec41761e47b933d524934d30144ad177c950
Hongwei Wang <hww712@gmail.com>
2013-08-09 02:54:12 -0700

    Update python sandbox for multi exception test.

--- python/sandbox/packagetest/main.py
+++ python/sandbox/packagetest/main.py
@@ -3,8 +3,26 @@
 
 from pkg1.m1 import M1BC
 import pkg2
+import sys
+
+
+
+class T1(object):
+    def test1(self):
+        try:
+            self.test2()
+            print "hehe"
+        except:
+            print sys.exc_info()
+    
+    def test2(self):
+        self.test3()
+    
+    def test3(self):
+        raise NameError('HiThere')
 
 if __name__ == "__main__":
     M1BC()
+    T1().test1()
 
 
