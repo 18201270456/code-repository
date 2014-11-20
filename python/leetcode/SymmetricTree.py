0e82cd5f96c208d452dbddd8456b985662718b8b
Henry.Wang <skymatrix@126.com>
2014-11-20 17:45:53 +0800

    Add leetcode.

---
+++ python/leetcode/SymmetricTree.py
@@ -0,0 +1,97 @@
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+
+class Solution:
+    # @param root, a tree node
+    # @return a boolean
+    def isSymmetric(self, root):  # recursively 
+        if root == None:
+            return True
+        
+        return self.is_mirrored(root.left, root.right)
+    
+    
+    def is_mirrored(self, left, right):
+        if left == None and right == None:
+            return True
+        
+        if left != None and right != None:
+            if left.val == right.val:
+                return self.is_mirrored(left.left, right.right) and self.is_mirrored(right.right, left.left)
+        
+        return False
+    
+    
+    
+    
+    def isSymmetric2(self, root):  # iteratively 
+        
+        
+        s = []
+        l = []
+        
+        s.append(root)
+        
+        l.append(root.left)
+        l.append(root.right)
+        
+        s.append(l)
+        
+        t = []
+        for item in l:
+            t.append(l.left)
+        
+        
+        serialization = []
+        while root != None:
+            serialization.append(root.left)
+            serialization.append(root.right)
+        
+        try:
+            while root != None:
+                if root.left.val == root.right.val:
+                    root = root.left
+                
+                
+        except:
+            return False
+        while root != None:
+            if root.left != None:
+                root.left.val == root.right.val
+    
+    
+    def traverse_tree(self, root):
+        
+        l = []
+        l.append(root)
+        
+        while ( l[-1] != None*len(l[-1]) ):
+            newlist = []
+            for item in l[-1]:
+                newlist.append(item.left)
+                newlist.append(item.right)
+            
+            
+            
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+
+
+
