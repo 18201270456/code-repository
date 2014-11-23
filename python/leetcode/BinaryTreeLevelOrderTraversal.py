6848f2ce2e570749000bed6b0748032005c20ec6
Henry.Wang <skymatrix@126.com>
2014-11-23 22:09:00 +0800

    update leetcode.

---
+++ python/leetcode/BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,27 @@
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def node_to_serialized(root):
+    serialized = []
+    
+    level = []
+    level.append(root.val)
+    
+    return serialized
+    
+    
+
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def levelOrder(self, root):
+        pass
+    
+
+
--- python/sandbox/sandbox_tmp.py
+++ python/sandbox/sandbox_tmp.py
@@ -1,5 +1,5 @@
 
-
+import mysql
 
 path = ""
 
